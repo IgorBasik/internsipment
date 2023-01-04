279e3d4b3c09961c24bf6ee2e06d77e4f93ce8b9
IgorBasik <91775270+IgorBasik@users.noreply.github.com>
2023-01-04 14:48:06 +0200

    first task

---
+++ Task#1 - conditional operator/SwitchCase.js
@@ -0,0 +1,35 @@
+let n = 2;
+switch (n) {
+    case 1:
+      console.log("one");
+      break;
+    case 2:
+      console.log("two");
+      break;
+    case 3:
+      console.log("three");
+      break;
+    case 4:
+      console.log("four");
+      break;
+    case 5:
+    case 6:
+      console.log("five or six");
+      break;
+      case 7:
+        console.log("seven");
+        break;
+      case 8:
+        console.log("eight");
+        break;
+      case 9:
+        console.log("nine");
+        break;
+      case 0:
+        console.log("zero");
+        break;
+
+    default:
+      console.log("another values");
+  }
+ 
