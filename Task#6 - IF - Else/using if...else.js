5d8a25df9735df4fe73a148640dd5ae22bd1322c
IgorBasik <91775270+IgorBasik@users.noreply.github.com>
2023-01-04 15:23:44 +0200

    changes in sixth task

--- Task#6 - IF - Else/using if...else.js
+++ Task#6 - IF - Else/using if...else.js
@@ -10,3 +10,19 @@ if (number === 0) {
  else {
     alert(error );
 }
+
+/* const number = prompt('write a number');
+if (number > 0) {
+    alert(1) ;}
+  if (number < 0) {
+    alert(-1);
+  }
+if (number === 0) {
+    alert(0);
+  }
+ else if (number = string) {
+    alert(error );
+}
+ else if (number = null) {
+    alert(error );
+}
