ba14d39e9691c9bef7473909773c8d25fb6f540f
IgorBasik <91775270+IgorBasik@users.noreply.github.com>
2023-01-05 19:03:43 +0200

    seventh task

--- Task#6 - IF - Else/using if...else.js
+++ Task#6 - IF - Else/using if...else.js
@@ -14,18 +14,3 @@ if(number){
 }
 
 
-/* const number = prompt('write a number');
-if (number > 0) {
-    alert(1) ;}
-  if (number < 0) {
-    alert(-1);
-  }
-if (number === 0) {
-    alert(0);
-  }
- else if (typeof number == "string") {
-    alert(error );
-}
- else if (number == null) {
-    alert(error );
-}
---
+++ Task#7 - conditional operator/Operator NOT.js
@@ -0,0 +1,17 @@
+//first etap
+const age = prompt('write your age ');
+    if (!(age >= 14 && age <= 90) ){
+        alert('The value of age is NOT between 14 and 90.');
+    } 
+    else {
+        alert('The value of age is a between 14 and 90.') ;
+    } 
+
+//second etap
+const age = prompt('write your age ');
+    if (age >= 14 && age <= 90) {
+        alert('The value of age is a between 14 and 90.') ;
+    } 
+    else {
+        alert('The value of age is NOT between 14 and 90.');
+    } 
